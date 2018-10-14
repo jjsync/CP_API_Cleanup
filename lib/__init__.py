d426021f6b0ffde5e37bb1a209307527da4c185c
jjsync <jjsmith1986@gmail.com>
2018-10-14 13:31:13 -0400

    Updated to utilize ReverseLookup class object

--- lib/__init__.py
+++ lib/__init__.py
@@ -3,3 +3,4 @@ from mgmt_api import APIClientArgs
 from api_exceptions import APIException
 from api_exceptions import APIClientException
 from pinger import Pinger
+from reversenamelookup import ReverseLookups
